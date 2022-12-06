81380f350cccd56ac256ca53aec49b37773fdc20
Lluis <lluisb3@gmail.com>
2022-12-06 20:28:48 +0100

    database folder with metadata.py

---
+++ database/metadata.py
@@ -0,0 +1,51 @@
+from pathlib import Path
+import nibabel as nib
+import numpy as np
+from utils import csv_writer
+
+
+def metadata_creation():
+    """
+
+    :return:
+    """
+    datadir = Path("data")
+    images_files_train = [i for i in datadir.rglob("*.nii.gz") if "Training" in str(i) and "seg" not in str(i)]
+    images_files_validation = [i for i in datadir.rglob("*.nii.gz") if "Validation" in str(i) and "seg" not in str(i)]
+    images_files_test = [i for i in datadir.rglob("*.nii.gz") if "Test" in str(i) and "seg" not in str(i)]
+    header = ['Name', 'Dataset', 'Original Size', 'Voxel Size (xyz)',
+              'DataType','Minimum intensity (non-zero)', 'Maximum intensity']
+    csv_writer('data/Metadata_IBSR18.csv', 'w', header)
+    for file in images_files_train:
+        brain_image = nib.load(file)
+        brain_data = np.squeeze(brain_image.get_fdata())
+        min_value = np.min(brain_data[brain_data.nonzero()])
+        max_value = np.max(brain_data)
+        row = [Path(file.stem).stem, 'Train', brain_image.shape[:3],
+               tuple(np.diag(brain_image.affine)[:-1]), brain_image.get_data_dtype(),
+               min_value, max_value]
+        # Notice that to remove both sufixxes, .nii.gz, a double stem was applied
+        csv_writer('data/Metadata_IBSR18.csv', 'a', row)
+
+    for file in images_files_validation:
+        brain_image = nib.load(file)
+        brain_data = np.squeeze(brain_image.get_fdata())
+        min_value = np.min(brain_data[brain_data.nonzero()])
+        max_value = np.max(brain_data)
+        row = [Path(file.stem).stem, 'Validation', brain_image.shape[:3],
+               tuple(np.diag(brain_image.affine)[:-1]), brain_image.get_data_dtype(),
+               min_value, max_value]
+        # Notice that to remove both sufixxes, .nii.gz, a double stem was applied
+        csv_writer('data/Metadata_IBSR18.csv', 'a', row)
+
+    for file in images_files_test:
+        brain_image = nib.load(file)
+        brain_data = np.squeeze(brain_image.get_fdata())
+        min_value = np.min(brain_data[brain_data.nonzero()])
+        max_value = np.max(brain_data)
+        row = [Path(file.stem).stem, 'Test', brain_image.shape[:3],
+               tuple(np.diag(brain_image.affine)[:-1]), brain_image.get_data_dtype(),
+               min_value, max_value]
+        # Notice that to remove both sufixxes, .nii.gz, a double stem was applied
+        csv_writer('data/Metadata_IBSR18.csv', 'a', row)
+    print('Metadata csv created at data/Metadata_IBSR18.csv')
--- utils/globalfunctions.py
+++ utils/globalfunctions.py
@@ -1,7 +1,5 @@
 import csv
 from pathlib import Path
-import nibabel as nib
-import numpy as np
 
 
 def csv_writer(filename, action, row):
@@ -21,50 +19,3 @@ def csv_writer(filename, action, row):
         writer = csv.writer(f)
         writer.writerow(row)
         f.close()
-
-
-def metadata_creation():
-    """
-
-    :return:
-    """
-    datadir = Path("data")
-    images_files_train = [i for i in datadir.rglob("*.nii.gz") if "Training" in str(i) and "seg" not in str(i)]
-    images_files_validation = [i for i in datadir.rglob("*.nii.gz") if "Validation" in str(i) and "seg" not in str(i)]
-    images_files_test = [i for i in datadir.rglob("*.nii.gz") if "Test" in str(i) and "seg" not in str(i)]
-    header = ['Name', 'Dataset', 'Original Size', 'Voxel Size (xyz)',
-              'DataType','Minimum intensity (non-zero)', 'Maximum intensity']
-    csv_writer('data/Metadata_IBSR18.csv', 'w', header)
-    for file in images_files_train:
-        brain_image = nib.load(file)
-        brain_data = np.squeeze(brain_image.get_fdata())
-        min_value = np.min(brain_data[brain_data.nonzero()])
-        max_value = np.max(brain_data)
-        row = [Path(file.stem).stem, 'Train', brain_image.shape[:3],
-               tuple(np.diag(brain_image.affine)[:-1]), brain_image.get_data_dtype(),
-               min_value, max_value]
-        # Notice that to remove both sufixxes, .nii.gz, a double stem was applied
-        csv_writer('data/Metadata_IBSR18.csv', 'a', row)
-
-    for file in images_files_validation:
-        brain_image = nib.load(file)
-        brain_data = np.squeeze(brain_image.get_fdata())
-        min_value = np.min(brain_data[brain_data.nonzero()])
-        max_value = np.max(brain_data)
-        row = [Path(file.stem).stem, 'Validation', brain_image.shape[:3],
-               tuple(np.diag(brain_image.affine)[:-1]), brain_image.get_data_dtype(),
-               min_value, max_value]
-        # Notice that to remove both sufixxes, .nii.gz, a double stem was applied
-        csv_writer('data/Metadata_IBSR18.csv', 'a', row)
-
-    for file in images_files_test:
-        brain_image = nib.load(file)
-        brain_data = np.squeeze(brain_image.get_fdata())
-        min_value = np.min(brain_data[brain_data.nonzero()])
-        max_value = np.max(brain_data)
-        row = [Path(file.stem).stem, 'Test', brain_image.shape[:3],
-               tuple(np.diag(brain_image.affine)[:-1]), brain_image.get_data_dtype(),
-               min_value, max_value]
-        # Notice that to remove both sufixxes, .nii.gz, a double stem was applied
-        csv_writer('data/Metadata_IBSR18.csv', 'a', row)
-    print('Metadata csv created at data/Metadata_IBSR18.csv')
