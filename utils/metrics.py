ed0f31ed9ac39cebdc925976692baa64f25a54c2
Lluis <lluisb3@gmail.com>
2022-12-28 18:34:31 +0100

    first segmentation multiatlas_segmentation.py with majority voting for all and for 1 in proces add metrics to compute DICE per tissue add in globalfunctions.py a function to read the groundtruth and save them in a dictionary

---
+++ utils/metrics.py
@@ -0,0 +1,39 @@
+import numpy as np
+from medpy.metric.binary import dc
+
+def dsc_score(prediction, groundtruth):
+    """
+    Dice score for brain volumens to the labels csf, wm and gm.
+    :param prediction: segmentation predicted
+    :param groundtruth: ground truth segmentation
+    :return: dice: list of the three dice scores, csf wm and gm
+    """
+    groundtruth = np.uint8(groundtruth)
+    wm_data_gt = groundtruth.copy()
+    gm_data_gt = groundtruth.copy()
+    csf_data_gt = groundtruth.copy()
+
+    # In the labels: 3->GRAY MATTER; 2-> WHITE MATTER; 1->CSF
+    wm_data_gt[wm_data_gt != 3] = 0
+    gm_data_gt[gm_data_gt != 2] = 0
+    csf_data_gt[csf_data_gt != 1] = 0
+
+    wm_data_PRED = prediction.copy()
+    gm_data_PRED = prediction.copy()
+    csf_data_PRED = prediction.copy()
+
+    wm_data_PRED[prediction != 3] = 0
+    gm_data_PRED[prediction != 2] = 0
+    csf_data_PRED[prediction != 1] = 0
+
+    dice_wm_PRED = dc(wm_data_PRED, wm_data_gt)
+    dice_gm_PRED = dc(gm_data_PRED, gm_data_gt)
+    dice_csf_PRED = dc(csf_data_PRED, csf_data_gt)
+
+    dice = [dice_csf_PRED, dice_wm_PRED, dice_gm_PRED]
+
+    print(f"\nDice score wm: {dice_wm_PRED}")
+    print(f"Dice score gm: {dice_gm_PRED}")
+    print(f"Dice score csf: {dice_csf_PRED}\n")
+
+    return dice
